f1071b1d5fb4f34a8e737121a159e769da30d854
HeenaNagda <heena.nagda91@gmail.com>
2025-02-09 23:38:26 +0000

    .

--- worker/src/global_order_maker.rs
+++ worker/src/global_order_maker.rs
@@ -222,17 +222,17 @@ impl GlobalOrderMaker {
 
                 // info!("global_order_maker::run : digest = {:?}, num of nodes = {:?}", debug_batch_digest, global_order_graph_obj.get_dag().node_count());
                 
-                for ((from, to), count) in &missed_edges{
-                    {
-                        let mut missed_edge_manager_lock = self.missed_edge_manager.lock().await;
-                        missed_edge_manager_lock.add_missing_edge(*from, *to).await;
-                        missed_edge_manager_lock.add_updated_edge(*from, *to, *count).await;
-                    }
+                // for ((from, to), count) in &missed_edges{
+                //     {
+                //         let mut missed_edge_manager_lock = self.missed_edge_manager.lock().await;
+                //         missed_edge_manager_lock.add_missing_edge(*from, *to).await;
+                //         missed_edge_manager_lock.add_updated_edge(*from, *to, *count).await;
+                //     }
 
-                    if !missed_pairs.contains(&(*to, *from)){
-                        missed_pairs.insert((*from, *to));
-                    }
-                }
+                //     if !missed_pairs.contains(&(*to, *from)){
+                //         missed_pairs.insert((*from, *to));
+                //     }
+                // }
 
                 info!("missed_pairs count = {:?}", missed_pairs.len());
                 // info!("global_order_maker::run : global_order_graph size = {:?}", global_order_graph.len());
